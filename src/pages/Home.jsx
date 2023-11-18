11d6fe22a030e9ebc2a8f00ea5d96db2dae6969f
chearin <kimchaerin9670@naver.com>
2023-11-18 15:53:10 +0900

    페이지 SEO 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
 import Main from '../components/section/Main'
 
 const Home = () => {
   return (
-    <Main>
+    <Main 
+      title="웹스토리보이 유튜브" 
+      description="웹스토리보이 유튜브에 오신것을 환영합니다."
+    >
       Home
     </Main>
   )
